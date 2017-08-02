1b4a2061498016ab434b49def46ff845d2647ff3
Narenlogi <Narenlogi@users.noreply.github.com>
2017-08-02 16:13:06 +0530

    Created dockerfile for asset-monitoring-service (#15)
    * Created dockerfile for asset-monitoring-service
    
    * Updated the dockerfile and added templates for config  files
    
    * Updated the application key with environment variable in application.conf.template file

---
+++ dockerfiles/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Copyright © 2017 Logistimo.
+
+#This file is part of Logistimo.
+
+#Logistimo software is a mobile & web platform for supply chain management and remote temperature monitoring in
+#low-resource settings, made available under the terms of the GNU Affero General Public License (AGPL).
+
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+#Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+#later version.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+#for more details.
+
+#You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+#<http://www.gnu.org/licenses/>.
+
+#You can be released from the requirements of the license by purchasing a commercial license. To know more about
+#the commercial license, please contact us at opensource@logistimo.com
+
+sed -ri "/Jad\/Jar download servlet/,+4d" $TOMCAT_HOME/webapps/ROOT/WEB-INF/web.xml \
+        && sed -ri "$ a tomcat.util.http.parser.HttpParser.requestTargetAllow=|{}" $TOMCAT_HOME/conf/catalina.properties
+
+if [[ "$SENTINEL_HOST" != "" ]]
+then
+envsubst < $TOMCAT_HOME/conf/context.xml.template > $TOMCAT_HOME/conf/context.xml
+fi
+
+envsubst < $TOMCAT_HOME/conf/server.xml.template > $TOMCAT_HOME/conf/server.xml
+
+envsubst < $TOMCAT_HOME/webapps/ROOT/WEB-INF/classes/application.conf.template  > $TOMCAT_HOME/webapps/ROOT/WEB-INF/classes/application.conf
+
+JAVA_OPTS="-Xms$JAVA_XMS -Xmx$JAVA_XMX \
+	 -\"javaagent://$TOMCAT_HOME/jmx_prometheus_javaagent-0.7.jar=$JMX_AGENT_PORT:$TOMCAT_HOME/jmx_exporter.json\""
+
+exec $TOMCAT_HOME/bin/catalina.sh run
